443336d4b7e2f821b96905496205fb5dc8037ea8
Michael Carr <mcarr2468@gmail.com>
2022-10-11 12:10:38 -0500

    added eerything back to how it was

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// pages
+import Home from './pages/Home'
+import Products from './pages/Products'
+import SingleProduct from './pages/SingleProduct'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Error from './pages/Error';
+// components
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
 
 
 function App() {
   let url = process.env.REACT_APP_GLASSES_DATA_URL
-  console.log(url)
 
   return (
-    <h2>nothing here</h2>
+    <BrowserRouter>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/products" element={ <Products/> } />
+        <Route path="/singleProduct/:productId" element={ <SingleProduct/> } />
+        <Route path="/cart" element={ <Cart/> } />
+        <Route path="/checkout" element={ <Checkout/> } />
+        <Route path="*" element={ <Error/> } />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// contexts
+import { ProductsProvider } from './contexts/products_context'
+import { FilterProvider } from './contexts/filter_context';
+import { CartProvider } from './contexts/cart_context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
 root.render(
   <React.StrictMode>
-    <App/>
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App/>
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
   </React.StrictMode>
 );
+
