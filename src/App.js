c7cd181f79f4411f614462b213b2dec70066cb0b
Michael Carr <mcarr2468@gmail.com>
2022-10-11 10:02:48 -0500

    remove eveything form App.js again

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
 
-// pages
-import Home from './pages/Home'
-import Products from './pages/Products'
-import SingleProduct from './pages/SingleProduct'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Error from './pages/Error';
-// components
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Footer from './components/Footer'
 
 
 function App() {
-  const [data, setData] = React.useState(null)
-
-  const fetchData = _ => {
-
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => setData(data)); 
-  }
-
-  React.useEffect(() => {
-    // fetchData()
-  }, [])
-
-
-  React.useEffect(() => {
-    console.log(data)
-  }, [data])
+  
 
   return (
-    <h2>no routes</h2>
+    <h2>nothing here</h2>
   );
 }
 
