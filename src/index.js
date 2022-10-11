447300b9829dc8171dc16ea468eec03e4752d744
Michael Carr <mcarr2468@gmail.com>
2022-10-11 12:25:06 -0500

    removed context and reducer

--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// contexts
-import { ProductsProvider } from './contexts/products_context'
-import { FilterProvider } from './contexts/filter_context';
-import { CartProvider } from './contexts/cart_context';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App/>
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <App/>
   </React.StrictMode>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import { useProductsContext } from '../contexts/products_context'
+
 
 
 const Home = () => {
-  const { 
-    isSidebarOpen,
-    openSidebar,
-    closeSidebar,
-  } = useProductsContext()
 
-  React.useEffect(() => {
-    openSidebar()
-  }, [])
-
-  React.useEffect(() => {
-    console.log(isSidebarOpen)
-  }, [isSidebarOpen])
 
 
   return (
