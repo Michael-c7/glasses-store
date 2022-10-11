a14a81c4f0e0ac0b2b68cc988b84c2099c90fee9
Michael Carr <mcarr2468@gmail.com>
2022-10-11 09:32:15 -0500

    removed all the pages,css eveything in the App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-// pages
-import Home from './pages/Home'
-import Products from './pages/Products'
-import SingleProduct from './pages/SingleProduct'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Error from './pages/Error';
-// components
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Footer from './components/Footer'
-
 
 function App() {
-  const [data, setData] = React.useState(null)
-
-  const fetchData = _ => {
-    let url = process.env.REACT_APP_GLASSES_DATA_URL
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => setData(data)); 
-  }
-
-  React.useEffect(() => {
-    // fetchData()
-  }, [])
-
-
-  React.useEffect(() => {
-    console.log(data)
-  }, [data])
-
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Sidebar/>
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/products" element={ <Products/> } />
-        <Route path="/singleProduct/:productId" element={ <SingleProduct/> } />
-        <Route path="/cart" element={ <Cart/> } />
-        <Route path="/checkout" element={ <Checkout/> } />
-        <Route path="*" element={ <Error/> } />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <h2>test w/ removing everything</h2>
   );
 }
 
--- src/utility/misc.js
+++
@@ -1,5 +0,0 @@
-/*
-put miscellaneous function that have
-no specific place here
-eg: calculating a season, creating a date, ect...
-*/
