638caf3851b1e54d759a110a0144fd2e872a9382
Michael Carr <mcarr2468@gmail.com>
2022-10-11 14:53:19 -0500

    un-implemented the provider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { ProductsProvider } from './context/products_context'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <App/>
-    </ProductsProvider>
+    <App/>
   </React.StrictMode>
 );
 
