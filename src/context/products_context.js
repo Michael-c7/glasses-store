70229985ad54eacd330a0d619f97468517e6eb1b
Michael Carr <mcarr2468@gmail.com>
2022-10-11 15:17:33 -0500

    re-added product context reducer,ect...

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,33 +1,26 @@
 import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/products_reducer'
 
-import {
-  SIDEBAR_OPEN,
-  SIDEBAR_CLOSE,
 
-} from '../actions'
 
-const initialState = {
-  isSidebarOpen: false,
-}
+const initialState = {}
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
-
   return (
     <ProductsContext.Provider
       value={{
         ...state,
-
       }}
     >
       {children}
     </ProductsContext.Provider>
   )
 }
+
 
 export const useProductsContext = () => {
   return useContext(ProductsContext)
