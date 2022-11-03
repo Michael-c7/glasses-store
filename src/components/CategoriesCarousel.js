cc72a8cf04ba5fce0547fc78ae83683eece51fd6
Michael Carr <mcarr2468@gmail.com>
2022-11-02 19:31:39 -0500

    started on the refactored version of the carousel

--- src/components/CategoriesCarousel.js
+++ src/components/CategoriesCarousel.js
@@ -47,76 +47,37 @@ const CategoriesCarousel = () => {
     ]
 
 
-    let cardsInSingleSlideAmt = 3;
-    let totalSlideAmt = Math.round(categoriesCarouselData.length / cardsInSingleSlideAmt)
-
-    
-    let slides = Array.from(({length:totalSlideAmt})).map((_, index) => {
-        if(index === 0) {
-            return [
-                categoriesCarouselData[0],
-                categoriesCarouselData[1],
-                categoriesCarouselData[2]
-            ]
-        } else if(index === 1) {
-            return [
-                categoriesCarouselData[3],
-                categoriesCarouselData[4],
-                categoriesCarouselData[5]
-            ]
-        } else {
-            throw Error('index not found')
-        }
-    })
-
-
-    
-
-
-    const changeSlide = _ => {
-        if(currentSlideIndex === 0) {
-            setCurrentSlideIndex(1)
-
-        } else {
-            setCurrentSlideIndex(0)
-        }
-    }
-
-
   return (
     <Wrapper>
         <div className='parallax'></div>
         
-        <div className='categoriesCarousel-container'>
+        <div className='categoriesCarousel__outer-container'>
             <h2 className='categoriesCarousel__heading'>Categories</h2>
-            <div className='categoriesCarousel__slides-outer-container'>
-                <button className='categoriesCarousel__slider-btn categoriesCarousel__prev-btn' onClick={changeSlide}>
+
+            <div className='categoriesCarousel__inner-container'>
+                <button className='categoriesCarousel__slider-btn categoriesCarousel__prev-btn'>
                     <GrPrevious/>
                 </button>
-                <div className='categoriesCarousel__slides-container'>
-                    <ul className='categoriesCarousel__slides'>
-                        {slides.map((_, index) => {
-                            return (
-                                <li className={`categoriesCarousel__slide ${currentSlideIndex === index ? 'current-slide' : 'prev-slide'}`} key={index}>
-                                    {slides[index].map((data, index) => {
-                                        return (
-                                            <Link className='categoriesCarousel__link' to='/products' key={index}>
-                                                <img className='categoriesCarousel__slide-img' src={data.image} alt={data.heading}/>
-                                                <div className='categoriesCarousel__heading-container'>
-                                                    <h2 className='categoriesCarousel__slide-heading'>{data.heading}</h2>
-                                                </div>
-                                            </Link>
-                                        )
-                                    })}
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-                <button className='categoriesCarousel__slider-btn categoriesCarousel__next-btn' onClick={changeSlide}>
+
+                <button className='categoriesCarousel__slider-btn categoriesCarousel__next-btn'>
                     <GrNext/>
                 </button>
 
+
+                <ul className='categoriesCarousel__slides'>
+                    {categoriesCarouselData.map((element) => {
+                        return (
+                            <li className='categoriesCarousel__slide'>
+                                <Link to='products' className='categoriesCarousel__slide__link'>
+                                    <img className='categoriesCarousel__slide-img' src={element.image} alt={element.heading}/>
+                                    <div className='categoriesCarousel__heading-container'>
+                                        <h2 className='categoriesCarousel__slide-heading'>{element.heading}</h2>
+                                    </div>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     </Wrapper>
@@ -126,188 +87,145 @@ const CategoriesCarousel = () => {
 export default CategoriesCarousel
 
 const Wrapper = styled.section`
-    --categoriesCarouselHeight:600px;
+        --categoriesCarouselHeight:600px;
+        --card-width:400px;
+        --card-amt-shown:3;
+        --card-outer-width:calc(var(--card-width) * var(--card-amt-shown));
 
-    position:relative;
-    height:var(--categoriesCarouselHeight);
+        position:relative;
+        height:var(--categoriesCarouselHeight);
 
 
 
-    .parallax {
-        height:100%;
-        background-image: url(${parallaxImage});
-    /* creates the parallax scrolling effect */
-        background-attachment: fixed;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-    // to make image less noticeable
-        filter: brightness(0.25) grayscale(100%);
-        box-shadow: inset 0 0 100px #111;   
-    }
-
-    // removes the parallax effect for mobile
-    @media only screen and (max-device-width: 1024px) {
         .parallax {
-          background-attachment: scroll;
+            height:100%;
+            background-image: url(${parallaxImage});
+        /* creates the parallax scrolling effect */
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        // to make image less noticeable
+            filter: brightness(0.25) grayscale(100%);
+            box-shadow: inset 0 0 100px #111;   
         }
-      }
 
-
-
-
-
-
-
-
-      .categoriesCarousel-container {
-        position:absolute;
-        top:0;
-        z-index:1;
-        left:50%;
-        top:50%;
-        transform:translate(-50%, -50%);
-        width:1000px;
-        height:100%;
-    }
-
-    .categoriesCarousel__heading {
-        text-align:center;
-        font-size:2.25rem;
-        font-weight:500;
-        color:#fff;
-        position:absolute;
-        z-index:10;
-        left:50%;
-        transform:translateX(-50%);
-        margin-top:2rem;
-    }
-
-
-    .categoriesCarousel__slides-outer-container {
-        overflow:hidden;
-    }
-
-
-    .categoriesCarousel__slider-btn  {
-        position:absolute;
-        top:50%;
-        transform:translateY(-50%);
-        width:50px;
-        height:50px;
-        border:none;
-        font-size:1.5rem;
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        border-radius:100px;
-        cursor:pointer;
-    }
-
-    .categoriesCarousel__prev-btn {
-        left:0;
-        margin-left:-4rem;
-    }
-
-    .categoriesCarousel__next-btn {
-        right:0;
-        margin-right:-4rem;
-    }
-
-    .categoriesCarousel__slides {
-        position:relative;
-        display:flex;
-        width:100%;
-        height:var(--categoriesCarouselHeight);  
-    }
-    
-    .categoriesCarousel__slide {
-        position:absolute;
-        display:flex;
-        width:100%;
-        height:65%;
-        gap:2rem;
-        top:50%;
-        transform:translateY(-50%);
-        transition:0.5s ease;
-    }
-
-    .prev-slide {
-        transform:translateY(-50%) translateX(100%);
-    }
-
-    .current-slide {
-        transform:translateY(-50%) translateX(0%);
-        z-index:10;
-    }
-
-    .categoriesCarousel__link {
-        flex:1;
-        position:relative;
-    }
-
-    .categoriesCarousel__slide-img {
-        width:100%;
-        height:100%;
-        object-fit:cover;
-    }
-
-    .categoriesCarousel__heading-container {
-        position:absolute;
-        background:rgba(25,25,25,0.75);
-        bottom:0;
-        width:100%;
-        left:0;
-    }
-
-    .categoriesCarousel__slide-heading {
-        font-size:1.25rem;
-        font-weight:400;
-        padding:0.5rem 0;
-        color:var(--white);
-        text-align:center;
-    }
-
-
-
-
-
-
-    // mobile view
-    @media (max-width: 1200px) {
-        .categoriesCarousel-container {
-            transform:translate(-50%, -50%) scale(0.85);
+        // removes the parallax effect for mobile
+        @media only screen and (max-device-width: 1024px) {
+            .parallax {
+            background-attachment: scroll;
+            }
         }
-    }
 
-    @media (max-width: 1024px) {
-        .categoriesCarousel-container {
-            transform:translate(-50%, -50%) scale(0.65);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        .categoriesCarousel__outer-container {
+            // background:rgba(0,155,0,0.2);
+            position:absolute;
+            top:0;
+            color:#fff;
+            width:var(--card-outer-width);
+            left:50%;
+            top:0%;
+            transform:translate(-50%, -0%);
+            height:600px;
         }
-    }
 
-    @media (max-width: 768px) {
-        .categoriesCarousel-container {
-            transform:translate(-50%, -50%) scale(0.50);
+
+        .categoriesCarousel__heading {
+            text-align:center;
+            margin:1.5rem 0;
+            text-align:center;
+            font-size:2.25rem;
+            font-weight:500;
+            color:#fff;
         }
-    }
 
-    @media (max-width: 600px) {
-        // --categoriesCarouselHeight:400px;
 
-        .categoriesCarousel-container {
-            transform:translate(-50%, -50%) scale(0.35);
+
+        .categoriesCarousel__inner-container {
+            overflow:hidden;
+
         }
-    }
 
-    @media (max-width: 435px) {
-        // --categoriesCarouselHeight:100px;
 
-        .categoriesCarousel-container {
-            transform:translate(-50%, -50%) scale(0.25);
+        .categoriesCarousel__slider-btn  {
+            position:absolute;
+            width:50px;
+            height:50px;
+            border:none;
+            font-size:1.5rem;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            border-radius:100px;
+            cursor:pointer;
+            z-index:10;
+
+            top:50%;
+            transform:translateY(-50%);
         }
-    }
+
+        .categoriesCarousel__prev-btn {
+            left:0;
+            margin-left:-4rem;
+        }
+        .categoriesCarousel__next-btn {
+            right:0;
+            margin-right:-4rem;
+        }
 
 
+        .categoriesCarousel__slides {
+            position:absolute;
+            display:flex;
+            height:500px;
+            // gap:1rem;
+            
+        }
+        .categoriesCarousel__slide {
+            position:relative;
+            width:var(--card-width);
+            height:100%;
+            
+        }
+
+
+        .categoriesCarousel__slide-img {
+            width:100%;
+            height:100%;
+            object-fit:cover;
+        }
+
+        .categoriesCarousel__heading-container {
+            position:absolute;
+            background:rgba(25,25,25,0.75);
+            bottom:0;
+            width:100%;
+            left:0;
+        }
+
+        .categoriesCarousel__slide-heading {
+            font-size:1.25rem;
+            font-weight:400;
+            padding:0.5rem 0;
+            color:var(--white);
+            text-align:center;
+        }
 
 `
