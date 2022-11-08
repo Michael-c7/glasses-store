5d69fc6bfc1c929b4ce198eaf96762d7900327c5
Michael Carr <mcarr2468@gmail.com>
2022-11-08 17:37:22 -0600

    created the getProducts function

--- src/contexts/products_context.js
+++ src/contexts/products_context.js
@@ -5,11 +5,18 @@ import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
 
+  GET_PRODUCTS_START,
+  GET_PRODUCTS,
+  IS_PRODUCTS_ERROR,
+
 } from '../actions'
 
 const initialState = {
   isSidebarOpen: false,
-
+  products:[],
+  isProductsLoading:false,
+  isProductsError:false,
+  
 }
 
 const ProductsContext = React.createContext()
@@ -22,10 +29,30 @@ export const ProductsProvider = ({ children }) => {
     document.body.style.overflow = 'hidden'
     
   }
+
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE })
     document.body.style.overflow = 'visible'
   }
+
+  const getProducts = async (url) => {
+    dispatch({type: GET_PRODUCTS_START})
+    try {
+      const data = await fetch(url).then((res) => res.json())
+      dispatch({type:GET_PRODUCTS, payload:data.records})
+
+    } catch(error) {
+      dispatch({type:IS_PRODUCTS_ERROR})
+      console.log(error.message)
+    }
+  }
+
+
+  React.useEffect(() => {
+    getProducts(process.env.REACT_APP_GLASSES_DATA_URL)
+  }, [])
+
+  
 
   return (
     <ProductsContext.Provider
@@ -39,7 +66,10 @@ export const ProductsProvider = ({ children }) => {
     </ProductsContext.Provider>
   )
 }
-// make sure use
+
+
+
+// make sure to use this hook
 export const useProductsContext = () => {
   return useContext(ProductsContext)
 }
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,6 +1,9 @@
 import {
     SIDEBAR_OPEN,
     SIDEBAR_CLOSE,
+    GET_PRODUCTS_START,
+    GET_PRODUCTS,
+    IS_PRODUCTS_ERROR,
   } from '../actions'
   
   const products_reducer = (state, action) => {
@@ -10,6 +13,29 @@ import {
     if (action.type === SIDEBAR_CLOSE) {
       return { ...state, isSidebarOpen: false }
     }
+
+    /*
+    GET_PRODUCTS
+    IS_PRODUCTS_ERROR
+    */
+
+    if(action.type === GET_PRODUCTS_START) {
+      return {...state, isProductsLoading:true}
+    }
+
+    if(action.type === GET_PRODUCTS) {
+      return {
+        ...state,
+        products:action.payload,
+        isProductsLoading:false,
+        isProductsError:false,
+      }
+    }
+
+    if(action.type === IS_PRODUCTS_ERROR) {
+      return {...state,  isProductsError:true, isProductsLoading:false}
+    }
+
     throw new Error(`No Matching "${action.type}" - action type`)
   }
   
