744bf4e82289119bae18d1c0b9d3410338ecddfd
Michael Carr <mcarr2468@gmail.com>
2022-11-11 16:04:13 -0600

    implemented basic loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Loading = () => {
+  return (
+    <Wrapper>Loading...</Wrapper>
+  )
+}
+
+export default Loading
+
+const Wrapper = styled.section`
+    font-size:2.5rem;
+    text-align:center;
+    margin:0.5rem 0;
+
+`
