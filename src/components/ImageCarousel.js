19729066a24fd42bfdd9e016aef417d14d563ddd
Michael Carr <mcarr2468@gmail.com>
2022-11-15 17:52:40 -0600

    moved the imageCarouselData back to the ImageCarousel because it did not make sense to have in the reusable.js because it does not get reused anywhere

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -3,7 +3,42 @@ import styled from 'styled-components'
 import { Button1 } from '../styledComponents/Button1'
 import { Link } from 'react-router-dom'
 import { GrPrevious, GrNext } from 'react-icons/gr'
-import { imageCarouselData } from '../utility/reusable'
+
+import imageCarouselDataImage1 from '../assets/image_carousel_images/christian-buehner-DItYlc26zVI-unsplash.jpg'
+import imageCarouselDataImage2 from '../assets/image_carousel_images/christina-wocintechchat-com-50TkCaP8M3A-unsplash.jpg'
+import imageCarouselDataImage3 from '../assets/image_carousel_images/AdobeStock_534825688.jpeg'
+import imageCarouselDataImage4 from '../assets/image_carousel_images/AdobeStock_86658618.jpeg'
+
+
+
+const imageCarouselData = [
+  {
+      image:imageCarouselDataImage1,
+      topHeading:'New Arrivals',
+      mainHeading:'Designer Collection',
+      subHeading:'Up to 25% off on all designer glasses',
+  },
+  {
+      image:imageCarouselDataImage2,
+      topHeading:`Spring ${new Date().getFullYear()}`,
+      mainHeading:'New Collection',
+      subHeading:'Up to 50% off on Eyeglasses',
+  },
+  {
+    image:imageCarouselDataImage3,
+    topHeading:`Great Deals`,
+    mainHeading:"Men's Collection",
+    subHeading:'Affordable glasses for everybody',
+  },
+  {
+    image:imageCarouselDataImage4,
+    topHeading:`Best Deals`,
+    mainHeading:"Women's Collection",
+    subHeading:'Up to 75% off on Sunglasses',
+  },
+]
+
+
 
 
 const ImageCarousel = () => {
