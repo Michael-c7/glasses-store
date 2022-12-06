5a248e287bfb95f4b156883654c86a0c83de6c34
Michael Carr <mcarr2468@gmail.com>
2022-12-06 08:03:33 -0600

    added a function to generate a unique id

--- src/utility/misc.js
+++ src/utility/misc.js
@@ -1,5 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
 /*
 put miscellaneous function that have
 no specific place here
 eg: calculating a season, creating a date, ect...
 */
+
+
+export const generateUniqueId = _ => uuidv4()
