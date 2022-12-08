7afd3b5b9c5bef95211fd45d61b8d1fa02108e0e
Michael Carr <mcarr2468@gmail.com>
2022-12-08 09:52:06 -0600

    added the getNotUnique function

--- src/utility/misc.js
+++ src/utility/misc.js
@@ -9,3 +9,11 @@ eg: calculating a season, creating a date, ect...
 
 
 export const generateUniqueId = _ => uuidv4()
+
+
+
+export function getNotUnique(array) {
+    var map = new Map();
+    array.forEach(a => map.set(a, (map.get(a) || 0) + 1));
+    return array.filter(a => map.get(a) > 1);
+}
