9859b79e8cc5912a057de7a14abbafe6b61bad44
Michael Carr <mcarr2468@gmail.com>
2022-12-28 10:43:50 -0600

    added logic to add an item to cart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -1,11 +1,29 @@
 import {
-
+  ADD_PRODUCTS_TO_CART,
   } from '../actions'
 
 
   const cart_reducer = (state, action) => {
+
+    if(action.type === ADD_PRODUCTS_TO_CART) {
+        let productJustAdded = action.payload
+        let cartProducts = state.productsInCart
+
+        let list = [...cartProducts, productJustAdded]
+
+        const group = {}
+
+        list.forEach(e => {    
+          const o = group[e.product.id] = group[e.product.id] || {...e, amount: 0}
+          o.amount += e.amount
+       })
+       
+       const res = Object.values(group)
+
+       return {...state, productsInCart:res}
+    }
     
-    return state
+
 
     throw new Error(`No Matching "${action.type}" - action type`)
 
