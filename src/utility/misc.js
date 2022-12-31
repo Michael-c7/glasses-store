921ac08f0f9eb86e3807a8cf134c535c590bbbfb
Michael Carr <mcarr2468@gmail.com>
2022-12-31 11:02:58 -0600

    added toCurrency function that allows a number to be returned as a specific country / currency format

--- src/utility/misc.js
+++ src/utility/misc.js
@@ -11,9 +11,26 @@ eg: calculating a season, creating a date, ect...
 export const generateUniqueId = _ => uuidv4()
 
 
-
 export function getNotUnique(array) {
     var map = new Map();
     array.forEach(a => map.set(a, (map.get(a) || 0) + 1));
     return array.filter(a => map.get(a) > 1);
 }
+
+
+/*
+Take a number and return specified currency formatting.
+Allows for country / currency sensitive formatting
+
+Some examples
+    toCurrency(123456.789, 'EUR'); // €123,456.79  | currency: Euro | currencyLangFormat: Local
+    toCurrency(123456.789, 'USD', 'en-us'); // $123,456.79  | currency: US Dollar | currencyLangFormat: English (United States)
+    toCurrency(322342436423.2435, 'JPY'); // ¥322,342,436,423 | currency: Japanese Yen | currencyLangFormat: Local
+    toCurrency(322342436423.2435, 'JPY', 'fi'); // 322 342 436 423 ¥ | currency: Japanese Yen | currencyLangFormat: Finnish
+
+Documentation
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+
+*/
+export const toCurrency = (n, curr, LanguageFormat = undefined) =>
+  Intl.NumberFormat(LanguageFormat, { style: 'currency', currency: curr }).format(n);
