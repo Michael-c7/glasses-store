7b615c1524dd918d519f78ddd1f5aa4e51af0967
Michael Carr <mcarr2468@gmail.com>
2022-12-31 10:57:53 -0600

    added the update_cart_amount action and created the changeCartItemAmount function

--- src/contexts/cart_context.js
+++ src/contexts/cart_context.js
@@ -3,12 +3,16 @@ import React, { useEffect, useContext, useReducer } from 'react'
 import reducer from '../reducers/cart_reducer'
 
 
-// import {
+import {
+  ADD_PRODUCTS_TO_CART,
+  UPDATE_CART_AMOUNT,
+  } from '../actions'
 
-//   } from '../actions'
 
-
-const initialState = {}
+const initialState = {
+  productsInCart:[],
+  subTotal:0,
+}
 
 const CartContext = React.createContext()
 
@@ -16,13 +20,50 @@ const CartContext = React.createContext()
 export const CartProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
+
+    const addProductToCart = (product, amount = 1) => {
+      dispatch({ type: ADD_PRODUCTS_TO_CART, payload:{product, amount} })
+    }
+
+
+    const changeCartItemAmount = (itemId, itemAmount) => {
+      dispatch({ type:UPDATE_CART_AMOUNT, payload:{itemId, itemAmount} })
+    }
+
+    const removeCartItem = (itemId) => {
+      console.log('removeCartItem')
+    }
+
+    const calculateSubTotal = () => {
+      console.log('calculateSubTotal')
+            /*
+      1. unit price of each item * them quantity(eg: item costs $20, have 4 of them 4 * 20)
+      2. do this for all items
+      3. add the cost of all items up,this the sub-total
+      4. add flat shipping rate ($5) to sub total, this is the total
+      */
+    }
+
+
+    // React.useEffect(() => {
+    //   console.log(state.productsInCart)
+    // }, [state])
+
     return (
         <CartContext.Provider
           value={{
              ...state,
+             addProductToCart,
+             changeCartItemAmount,
         
         }}>
           {children}
         </CartContext.Provider>
       )
+}
+
+
+// make sure use
+export const useCartContext = () => {
+  return useContext(CartContext)
 }
