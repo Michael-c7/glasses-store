8eea5feb8bc04bfb3a282ddf301edbc10a7c933f
Michael Carr <mcarr2468@gmail.com>
2023-01-09 18:18:56 -0600

    minor formatting

--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ import { ProductsProvider } from './contexts/products_context'
 import { FilterProvider } from './contexts/filter_context';
 import { CartProvider } from './contexts/cart_context';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App/>
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App/>
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
   </React.StrictMode>
 );
